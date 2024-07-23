d8dfac17aab9dbcaadeb97167bf1f71a3a64ec70
Shanil Sanchez <shanils857@gmail.com>
2024-07-22 22:10:52 -0400

    Completed Todo List

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+
 export default class Todo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: "",
+    };
+  }
+
   render() {
+    const { id, name, completed } = this.props.todo
+    const { toggleCompleted } = this.props
     return (
-      <div>
-        Todo
+      <div onClick={() => toggleCompleted(id)}>
+        {name} {completed &&  '✔'}
       </div>
     )
   }
